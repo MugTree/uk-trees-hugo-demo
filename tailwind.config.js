bc3aae200e6b1200b4b7cc85f45728f483a80132
MugTree <waterhose@protonmail.com>
2024-10-10 19:07:08 +0100

    Added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./layouts/**/*.html", "./content/**/*.md"],
+  theme: {
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            "code::before": { content: "" },
+            "code::after": { content: "" },
+          },
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
