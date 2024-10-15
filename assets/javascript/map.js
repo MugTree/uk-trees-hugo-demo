0ed301eee6ad1f27c9e51b52cdd48ac1a69c4ac6
MugTree <waterhose@protonmail.com>
2024-10-15 16:13:48 +0100

    Updated map data

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -6,12 +6,17 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 let bounds = [];
+
 for (let i = 0; i < markers.length; i++) {
-  const marker = L.marker([markers[i].latitude, markers[i].longitude]).addTo(
-    map
-  );
-  marker.bindPopup(markers[i].name);
+  // Create popup for each marker
+  const popup = L.popup()
+    .setLatLng([markers[i].latitude, markers[i].longitude])
+    .setContent(markers[i].name) // Use the name from markers array
+    .addTo(map); // Add popup to map
+
+  // Add the marker position to bounds array for fitting the map
   bounds.push([markers[i].latitude, markers[i].longitude]);
 }
 
-map.fitBounds(bounds);
+// Fit map to bounds after adding all markers
+map.fitBounds(bounds).zoomOut(1);
